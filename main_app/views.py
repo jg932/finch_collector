fa649473b7ff6a39adbd17c84207c56b04b0aa53
jg932 <jared.h.goldstein@gmail.com>
2021-11-20 21:20:46 -0500

    through part 5

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,64 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello finches!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
-  return render(request, 'cats/index.html', { 'finches': finches })
+  return render(request, 'finches/index.html', { 'finches': finches })
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', {
+    'finch': finch, 'feeding_form': feeding_form
+    })
+
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('finches_detail', finch_id=finch_id)
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = '__all__'
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches/'
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
+finches = [
+  
+]
