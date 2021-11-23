5153b1936089eed6aa0acd469f9c1f4c514e4add
jg932 <jared.h.goldstein@gmail.com>
2021-11-23 14:28:43 -0500

    auth works

--- finch_collector/settings.py
+++ finch_collector/settings.py
@@ -123,4 +123,10 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+LOGIN_URL = 'home'
+
+LOGIN_REDIRECT_URL = 'finches_index'
+
+LOGOUT_REDIRECT_URL = 'home'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
